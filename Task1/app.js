f8118b6f2d8340fdd39c62ec134ac608de41cee5
ULadzimir Maskaliou <ULadzimir_Maskaliou@epam.com>
2019-02-17 21:07:54 +0300

    Homework 1 Node.JS Modules. NPM

---
+++ Task1/app.js
@@ -0,0 +1,10 @@
+import info from './config/appInfo.json';
+import {User, Product} from './models/helper';
+
+console.log(`${info.name}`)
+const Person = new User();
+const Goods = new Product();
+
+
+
+
